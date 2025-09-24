6a02897bc733f6e41f00ee86efc3093e871754e0
Breno PZ <d2023001951@unifei.edu.br>
2025-09-24 18:07:18 -0300

    app-financas

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 import { AppRegistry } from 'react-native';
 import App from './AppNavigation';
 import App1 from './AppFirebase'
+import AppFinancas from './App-Financas/App'
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App1);
+AppRegistry.registerComponent(appName, () => AppFinancas);
